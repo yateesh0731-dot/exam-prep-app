756994f5327630ad24f14cf7152bdad00008ba0c
yateesh0731-dot <yateesh0731@gmail.com>
2026-03-25 20:06:39 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="My Exam Vault", page_icon="📖", layout="wide")
 # 2. Sidebar Navigation
 st.sidebar.title("📚 Study Vault")
 exam_choice = st.sidebar.selectbox("Exam Type", ["UPSC Prelims", "SSC CGL"])
-subject = st.sidebar.radio("Subject", ["Polity", "Maths", "History"])
+subject = st.sidebar.radio("Subject", ["Polity", "Maths", "History", "Economy"])
 
 # 3. Simple Path Logic
 folder_map = {"UPSC Prelims": "upsc", "SSC CGL": "ssc"}
@@ -15,6 +15,7 @@ file_map = {
     "Polity": "president_of_india.md",
     "Maths": "percentages.md",
     "History": "history_notes.md"
+    "Economy": "economy_notes.md"
 }
 
 path = f"content/{folder_map[exam_choice]}/{subject.lower()}/{file_map[subject]}"
